8bc1f5279a114a493d734d15ef79506d4759cac2
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2022-06-17 12:25:58 -0300

    usages types

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use std::thread;
 use std::time::Duration;
 use psutil;
 use psutil::cpu::CpuPercentCollector;
+use psutil::{disk, memory};
+use psutil::disk::DiskUsage;
+use psutil::network::NetIoCountersCollector;
 
 const TIME: Duration = Duration::from_secs(1);
 
@@ -17,11 +20,29 @@ fn main() {
     // LOOK INTO: https://crates.io/crates/crossterm
 
     let mut cpupc: CpuPercentCollector = CpuPercentCollector::new().unwrap();
+    let mut netio = NetIoCountersCollector::default();
 
     loop {
         thread::sleep(TIME);
 
-        performance::cpu::get_cpu(&mut cpupc);
+        //performance::cpu::get_cpu(&mut cpupc);
+
+        let cpu_percents_percpu = cpupc.cpu_percent_percpu().unwrap();
+        let partitions = disk::partitions_physical().unwrap();
+        let disk_usages: Vec<DiskUsage> = partitions
+            .iter()
+            .map(|part| disk::disk_usage(part.mountpoint()).unwrap())
+            .collect();
+        let virtual_memory = memory::virtual_memory().unwrap();
+	    let swap_memory = memory::swap_memory().unwrap();
+        let net_io_counters = netio.net_io_counters().unwrap();
+
+        println!("cpu: {:?}", cpu_percents_percpu);
+        println!("disk: {:?}", disk_usages);
+        println!("ram: {:?}", virtual_memory);
+        println!("swap: {:?}", swap_memory);
+        println!("net: {:?}", net_io_counters);
+        println!()
     }
 
 }
