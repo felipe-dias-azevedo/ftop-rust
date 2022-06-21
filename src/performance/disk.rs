9c747282559604548ae6e0f962218adc6c43cbbc
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2022-06-21 15:21:22 -0300

    fixed partitions view macos

--- src/performance/disk.rs
+++ src/performance/disk.rs
@@ -5,12 +5,26 @@ use psutil::disk;
 use psutil::disk::{DiskUsage, Partition};
 use crate::console::byteutils::convert_bytes_to_giga_u32;
 
+const SYS: &str = std::env::consts::OS;
+
 pub struct DiskPartitionUsage {
     pub mount: String,
     pub volume: String,
     pub total: u32,
     pub usage: u32,
     pub percent: f32
+}
+
+fn get_disk_usage_by(partition: Partition) -> DiskPartitionUsage {
+    let disk_usage = get_disk_usage(partition.mountpoint()).unwrap();
+
+    DiskPartitionUsage {
+        mount: String::from(partition.device()),
+        volume: String::from(partition.mountpoint().to_str().unwrap_or("")),
+        total: convert_bytes_to_giga_u32(disk_usage.total()),
+        usage: convert_bytes_to_giga_u32(disk_usage.used()),
+        percent: disk_usage.percent()
+    }
 }
 
 pub fn get_disk_partitions_usage() -> Vec<DiskPartitionUsage> {
@@ -21,27 +35,27 @@ pub fn get_disk_partitions_usage() -> Vec<DiskPartitionUsage> {
 
     let partitions = get_disk_partitions().unwrap();
 
-    partitions.iter()
+    partitions.into_iter()
         .filter(|part| get_disk_usage(part.mountpoint()).is_some())
-        .map(|part| {
-
-            let disk_usage = get_disk_usage(part.mountpoint()).unwrap();
-
-            DiskPartitionUsage {
-                mount: String::from(part.device()),
-                volume: String::from(part.mountpoint().to_str().unwrap_or("")),
-                total: convert_bytes_to_giga_u32(disk_usage.total()),
-                usage: convert_bytes_to_giga_u32(disk_usage.used()),
-                percent: disk_usage.percent()
-            }
-        })
+        .map(get_disk_usage_by)
         .collect()
 }
 
 pub fn get_disk_partitions() -> Option<Vec<Partition>> {
     let partitions = disk::partitions_physical();
+
     match partitions {
-        Ok(v) => Some(v),
+        Ok(v) => {
+            if SYS != "macos" {
+                return Some(v)
+            }
+
+            let partitions = v.into_iter()
+                .filter(| part | "/" == part.mountpoint().to_str().unwrap_or(""))
+                .collect();
+
+            Some(partitions)
+        },
         Err(_err) => None
     }
 }
